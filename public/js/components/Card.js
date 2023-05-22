49b70a5c74419ee554e4ec90e3575024ca01b6d5
theinderbrar <90270249+theinderbrar@users.noreply.github.com>
2023-05-22 23:26:28 +0530

    added home page

---
+++ public/js/components/Card.js
@@ -0,0 +1,10 @@
+export default{
+    name:"Card",
+
+    template:
+        `
+        <div class="card">
+            <img src="images/playlist_1.jpg" alt="">
+        </div>
+        `
+}
---
+++ public/js/components/PopularSection.js
@@ -0,0 +1,19 @@
+import Card from '../components/Card.js'
+
+export default  {
+    template:
+        `
+        <div class="card-container">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+        </div>
+        `,
+    components: {
+        Card
+    }
+}
