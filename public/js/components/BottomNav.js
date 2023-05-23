f4a4d5a18ab9ca4f27a91d325f324a503137b66b
theinderbrar <90270249+theinderbrar@users.noreply.github.com>
2023-05-23 21:36:43 +0530

    changed icons of bottom navigation bar

--- public/js/components/BottomNav.js
+++ public/js/components/BottomNav.js
@@ -5,13 +5,20 @@ export default{
         `
         <div class="bottom-navbar">
             <a href="#" class="nav-item">
-                <i class="fa-solid fa-house"></i>
+                <i class="material-icons">home</i>
+                <p>Home</p>
             </a>
             <a href="#" class="nav-item">
-                <i class="fa-solid fa-house"></i>
+                <i class="material-icons">album</i>
+                <p>Albums</p>
             </a>
             <a href="#" class="nav-item">
-                <i class="fa-solid fa-house"></i>
+                <i class="material-icons">person</i>
+                <p>Artists</p>
+            </a>
+            <a href="#" class="nav-item">
+                <i class="material-icons">logout</i>
+                <p>Logout</p>
             </a>
         </div>
         `
